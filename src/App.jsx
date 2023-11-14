3b161b53f94bf8d38e98234707a326327352417f
Ajit33 <justajit33@gmail.com>
2023-11-14 06:04:58 +0530

    reduxtoolkit initialize done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import  config from "./conf/config"
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/AuthSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 function App() {
 
-  console.log(config.appwriteCollectiontId)
+ const [loading,setloading]=useState(true)
+ const dispatch=useDispatch()
 
-  return (
-    <>
-     <h1>Blog app</h1>
-    </>
-  )
+
+useEffect(()=>{
+  authService.getCurrentStatus()
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>setloading(false))
+},[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        { /* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) :<h1>Loading...</h1>
 }
 
 export default App
